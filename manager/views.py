f5533060db1bc2176cf72b17cf97fdfba1f7b380
gs0202 <hgmori@gmail.com>
2019-06-07 21:35:12 +0900

    foget

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,6 @@
+import json
+import random
+import request
 from django.shortcuts import render
 from manager.models import Person, Manager, Worker
 from django.views.generic import TemplateView
@@ -26,3 +29,15 @@ class WorkerListView2(TemplateView):
         workers = Worker.objects.filter(person__current_address=Person.SHIGA)
         context['workers'] = workers
         return render(self.request, self.template_name, context)
+
+def callback(request):
+    reply = ""
+    request_json = json.loads(request.body.decode('utf-8'))
+    for e in request_json['events']:
+        reply_token = e['replyToken']
+        message_type = e['message']['type']
+
+        if message_type == 'text':
+            text = e['message']['text']
+            reply += reply_text(reply_token, text)
+    return HttpResponse(reply)
